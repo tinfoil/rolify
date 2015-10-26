eee26c992e685d5d711d3179f2352c7b93ae2a40
Ben Sedat <bsedat@tinfoilsecurity.com>
2015-10-26 16:12:23 -0700

    Use ActiveSupport::Concern for Mongoid adapter monkeypatching

--- lib/rolify/railtie.rb
+++ lib/rolify/railtie.rb
@@ -2,17 +2,23 @@ require 'rolify'
 require 'rails'
 
 module Rolify
+  module MongoidAdapter
+    extend ActiveSupport::Concern
+
+    included do
+      extend Rolify
+    end
+  end
+
   class Railtie < Rails::Railtie
     initializer 'rolify.initialize' do
       ActiveSupport.on_load(:active_record) do
         ActiveRecord::Base.send :extend, Rolify
       end
-      
+
       config.before_initialize do
         ::Mongoid::Document.module_eval do
-          def self.included(base)
-            base.extend Rolify
-          end
+          include MongoidAdapter
         end
       end if defined?(Mongoid)
     end
